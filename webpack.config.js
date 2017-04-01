5333b8e3729c59a48f8c59b64793a851ef9d03dd
ChaniceStl <chanicestlouis2020@gmail.com>
2017-04-01 16:37:58 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
       { test: /\.scss$/, 
         use: ['style-loader', 'css-loader', 'sass-loader']  
       }
-     ],
+     ]
+   },
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx'],
