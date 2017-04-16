e9159d73b495d398de9bfa0b5e373ca2e2892ae5
victoramosjr <victoramosjr@gmail.com>
2017-04-16 02:13:01 -0400

    Database integration

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 app.get('/*', (req,res) => {
 	res.sendFile(path.join(__dirname, '../frontend/index.html'))
 })
 
-/*
-	process.env.PORT means = whatever is in the environment variable PORT, or 3000 if there's nothing there.
-*/
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is running at http://localhost:3000")
 })
