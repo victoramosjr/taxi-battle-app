08426e69fd4c658b9647e14adcd7731ab9f282d6
victoramosjr <victoramosjr@gmail.com>
2017-04-19 16:17:50 -0400

    Updates

--- backend/models/index.js
+++ backend/models/index.js
@@ -8,8 +8,8 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (process.env.NODE_ENV === 'production') {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
