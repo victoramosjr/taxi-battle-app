10486ab424287520e1093e5e2f8d28aef9dc888b
Chanice <chanicestlouis@aol.com>
2017-05-09 17:10:45 -0400

    Receiving token from Lyft

--- backend/routes/estimate-routes.js
+++ backend/routes/estimate-routes.js
@@ -1,40 +1,49 @@
 const router = require('express').Router();
-const $ = require('jquery');
+const axios = require('axios');
 
-function fetchLyftEstimate(token) {
-  $.ajax({
+const getLyftToken = (req, res, token) => {
+  console.log("URL===>", `https://api.lyft.com/v1/cost?start_lat=${req.params.currentLatitude}&start_lng=${req.params.currentLongitude}&end_lat=${req.params.destinationLatitude}&end_lng=${req.params.destinationLongitude}`)
+  axios({
     url: `https://api.lyft.com/v1/cost?start_lat=${req.params.currentLatitude}&start_lng=${req.params.currentLongitude}&end_lat=${req.params.destinationLatitude}&end_lng=${req.params.destinationLongitude}`,
-    headers: {
-      Authorization: 'Bearer ' + token
-    },
-    success: function(data) {
-      console.log(data)
+    headers: {  
+      Authorization: `Bearer ${token}`
     }
+  }).then(response => {
+    console.log(response.data)
+    res.send(response.data)
+  }).catch(error => {
+    res.send(error)
   })
 }
 
-$.ajax({
+const fetchLyftEstimate = (req, res) => {
+  console.log('api in backend')
+axios({
   url: 'https://api.lyft.com/oauth/token',
   method: 'POST',
   data: {"grant_type": "client_credentials", "scope": "public"},
   headers: {
     Authorization: 'Basic bEFmcmVMVHZXS1J4OnJ2TC00U3BuQXBwdHkzR2JyRDhnV0xkX0k5ZF9CQ1ZQ'
   },
-  username: 'lAfreLTvWKRx',
-  password: 'rvL-4SpnAppty3GbrD8gWLd_I9d_BCVP',
-  success: function(data) {
-    console.log(data.access_token)
-    getCost(data.access_token)
-  },
-  error: function(xhr, status, msg) {
-    console.log(xhr, status, msg);
+  auth:{
+    username: 'lAfreLTvWKRx',
+    password: 'rvL-4SpnAppty3GbrD8gWLd_I9d_BCVP'
   }
-});
+  
+}).then((response) => {
+    console.log(response.data.access_token)
+    getLyftToken(req, res, response.data.access_token)
+
+}).catch((error) => {
+    res.send(error)
+})
+
+}
 
 router.route('/lyft/:currentLatitude/:currentLongitude/:destinationLatitude/:destinationLongitude')
   .get(fetchLyftEstimate);
 
-router.route('/uber')
-  .get(fetchUberEstimate);
+// router.route('/uber')
+//   .get(fetchUberEstimate);
 
 module.exports = router;
